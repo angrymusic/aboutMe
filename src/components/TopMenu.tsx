b5dc9c18403007f09e5a1e84ffc28daae95b5acb
angrymusic <angrymusic@naver.com>
2023-02-07 14:54:00 +0900

    Create top menu bar and Set basic color

---
+++ src/components/TopMenu.tsx
@@ -0,0 +1,12 @@
+import '../styles/TopMenuStyle.css';
+function TopMenu() {
+
+    return (
+      <div className="top-menu-bar" >
+        
+      </div>
+    )
+  }
+  
+  export default TopMenu;
+  
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+
+function HomePage() {
+
+  return (
+    <div className="">
+      
+    </div>
+  )
+}
+
+export default HomePage
